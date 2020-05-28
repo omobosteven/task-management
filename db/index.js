7a746003e94898f860a95d07a637d0a0c3616c90
omobosteven <omobosteven@gmail.com>
2020-05-28 08:19:25 +0100

    chore(setup): setup koa and mongoose

---
+++ db/index.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/task_mgnt', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+export default db;
---
+++ index.js
@@ -0,0 +1,28 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import db from './db/index.js';
+
+const app = new Koa();
+const router = new Router();
+
+app.use(bodyParser());
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('database connected')
+});
+
+router.get('/', (ctx) => {
+  ctx.status = 200;
+  ctx.body = {
+    success: true,
+    message: "Welcome! To Task Management!"
+  }
+});
+
+
+app.use(router.routes())
+.use(router.allowedMethods());
+
+app.listen(3000, () => console.log('running on port 3000'));
