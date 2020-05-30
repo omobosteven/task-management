09659c88e96d4d8ee54146dd1800f0a6a5b6ab0a
omobosteven <omobosteven@gmail.com>
2020-05-30 01:37:53 +0100

    chore(setup): setup linting

--- index.js
+++ index.js
@@ -10,19 +10,17 @@ app.use(bodyParser());
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('database connected')
+  console.log('database connected');
 });
 
 router.get('/', (ctx) => {
   ctx.status = 200;
   ctx.body = {
     success: true,
-    message: "Welcome! To Task Management!"
-  }
+    message: 'Welcome! To Task Management!',
+  };
 });
 
-
-app.use(router.routes())
-.use(router.allowedMethods());
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3000, () => console.log('running on port 3000'));
