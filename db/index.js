9f05e1da98b638043119948767c31f93a4e841de
omobosteven <omobosteven@gmail.com>
2020-05-30 01:37:53 +0100

    chore(setup): setup linting

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 mongoose.connect('mongodb://localhost:27017/task_mgnt', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
