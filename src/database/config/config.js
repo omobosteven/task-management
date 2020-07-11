3dafffdf3463c39fc34afa4194cebd196816e734
omobosteven <omobosteven@gmail.com>
2020-07-11 23:46:07 +0100

    chore(setup): change to express and postgres - setup testing environment with jest

---
+++ src/database/config/config.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    logging: false,
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+  },
+};
+
+module.exports = config;
---
+++ src/routes/baseRouter.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const base = Router();
+
+base.get('', (req, res) => {
+  res.send({
+    success: true,
+    message: 'Welcome! To Task Management!',
+  });
+});
+
+export default base;
